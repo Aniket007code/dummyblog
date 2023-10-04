db9d99fc7d5dca55cd2fa35a404a4c909320f5e7
Aniket Mourya <aniketmourya77@gmail.com>
2023-10-04 16:14:59 +0530

    jjjjjjj

--- javascript Projexts/Github Profile App/app.js
+++ javascript Projexts/Github Profile App/app.js
@@ -1,10 +1,11 @@
+
 const APIURL = "https://api.github.com/users/"
 const displayArea = document.getElementById("displayArea")
 
 const getUser = async (username) => {
     const response = await fetch(APIURL + username)
     const data = await response.json()
-    console.log(data);
+    // console.log(data);
 
     const card = `
     <div class="imgSide">
@@ -22,13 +23,54 @@ const getUser = async (username) => {
     <div class="userInfo">
         <li>${data.followers} <b>Followers</b></li>
         <li>${data.following} <b>Following</b></li>
-        <li>17 <b>Repos</b></li>
+        <li>${data.public_repos} <b>Repos</b></li>
+    </div>
+
+    <div id = "repos">
+      
     </div>
 </div>`
-
+// <li>projects</li>
 displayArea.innerHTML = card;
+
+getRepos(username)
  
 }
 
 
-getUser("ashishsingh-30")
+getUser("Aniket007code")
+
+const getRepos = async(userName)=>{
+    const repos = document.getElementById("repos")
+    const response = await fetch(APIURL + userName + "/repos")
+    const data = await response.json();
+    // console.log(data);
+    data.forEach(
+        (item)=>{
+            console.log(item);
+            const elem = document.createElement("a");
+            elem.classList.add("rep");
+            elem.href = item.html_url;
+            elem.innerText = item.name;
+            elem.target = "_blank";
+            repos.appendChild(elem);
+
+
+        }
+    )
+
+}
+
+const formSubmit =() =>{
+    const inputText = document.querySelector("#inputText").value;
+    console.log(inputText);
+    if(inputText.value != ""){
+      getUser(inputText.value)
+    inputText.value="";
+
+
+    }
+
+    return false
+
+}
